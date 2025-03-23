b39daff6a311a347f954c0084fa326b1863fb3f2
Md Alkama <120254460+mdalkama@users.noreply.github.com>
2025-03-23 11:39:05 +0530

    skills, basic info all popup added popup position fixed

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -5,55 +5,75 @@ import { IoMdClose } from "react-icons/io";
 
 
 export default function Profile() {
-  const user = 1;
-  const profile = "https://i.pinimg.com/564x/3c/1c/73/3c1c7364ed3445e25235b032ebc1dfe5.jpg";
-  const name = "Md Alkama";
-  const email = "mdalkamadad@gmail.com";
-  const education = 1;
-  const education1 = {
-    name: "Maulana Azad College Of Engineering And Technlogy",
-    degree: "B.Tech",
-    field: "Computer Science Engineering",
-    startYear: "2020",
-    endYear: "2024",
-    location: "Patna, Bihar"
+  const det = 1;
+  const user = {
+    id: 1,
+    name: "Md Alkama",
+    email: "mdalkamadad@gmail.com",
+    phone: "1234567890",
+    age: 22,
+    role: "Mentor",
+    profile: "https://i.pinimg.com/564x/3c/1c/73/3c1c7364ed3445e25235b032ebc1dfe5.jpg",
+    location: "Patna, Bihar",
+    gender: "Male",
+    skills: {
+      0: "React",
+      1: "Node.js",
+      2: "JavaScript",
+      3: "Python",
+      4: "Django",
+      5: "HTML",
+      6: "CSS",
+      7: "MongoDB"
+    },
+    education: {
+      0:{
+        name: "Maulana Azad College Of Engineering And Technlogy",
+        degree: "B.Tech",
+        field: "Computer Science Engineering",
+        startYear: "2020",
+        endYear: "2024",
+        location: "Patna, Bihar"
+      }
+    }
   }
+  const skill = Object.values(user.skills);
   const [isOpenEditProfile, setIsOpenEditProfile] = useState(false);
+  const [isOpenSkillsAdd, setIsOpenSkillsAdd] = useState(false);
   const [scroll, setScroll] = useState(true);
 
     const scrollToggle = () => {
-      console.log(scroll)
     setScroll(!scroll);
   };
 
   const openEditProfileToggle = () => {
-    console.log(isOpenEditProfile);
-    
     setIsOpenEditProfile(!isOpenEditProfile);
+  }
+  const openSkillsAddToggle = () => {
+    setIsOpenSkillsAdd(!isOpenSkillsAdd);
   }
 { scroll ? document.body.style.overflow = "auto" : document.body.style.overflow = "hidden" }
 
-
   return (
-    <div className='mt-[68px] pt-4 flex flex-col items-center justify-start'>
+    <div className='mt-[68px] py-4 flex flex-col items-center justify-start'>
       {/* profile main */}
         <div id="profile-main" className='px-4 flex flex-col md:flex-row md:h-[200px] w-full md:w-[90%] justify-between items-center'>
           <div id='profile-and-info' className='flex md:h-full flex-col  md:flex-row justify-center items-center gap-4'>
-            <div style={{backgroundImage: `url(${profile})`}} id="profie-photo" className='h-[150px] w-[150px] rounded-full bg-cover bg-center'>
+            <div style={{backgroundImage: `url(${user.profile})`}} id="profie-photo" className='h-[150px] w-[150px] rounded-full bg-cover bg-center'>
             </div>
             <div id='name-and-email' className='flex flex-col items-center md:items-start'>
-              <p className='font-bold text-2xl'>{name}</p>
-              <p className='font-semibold text-lg'>{email}</p>
+              <p className='font-bold text-2xl'>{user.name}</p>
+              <p className='font-semibold text-lg'>{user.email}</p>
             </div>
           </div>
 
           <div className='w-full md:w-[150px] flex justify-center items-center'>
             {
-              user === 1 
+              det === 1 
               ? 
               <button onClick={()=>{openEditProfileToggle(); setScroll(!scroll); }} className=' h-[50px] w-[90%] md:w-[150px] rounded-md mt-4  md:mt-0 bg-[#052E16] text-white'>Edit Profile</button> 
               :
-              user === 2 
+              det === 2 
               ? 
               <button className=' h-[50px] w-[90%] md:w-[150px] rounded-md mt-4  md:mt-0 bg-[#0B65C2] text-white'>Connect</button> 
               : 
@@ -62,11 +82,14 @@ export default function Profile() {
           </div>
 
         </div>
-        {/* education section */}
-        <Education education = {education} education1 = {education1} />
+        
+        {/* basic info section */}
+        <BasicInfo user = {user} />
+
 
         {/* education section */}
-        <Education education = {education} education1 = {education1} />
+        <Education education = {user.education} education1 = {user.education} />
+
 
         {/* edit profile popup */}
         {
@@ -77,41 +100,36 @@ export default function Profile() {
         null
         }
 
+
+        {/* Skills section */}
+        <Skills skills = {skill} openSkillsAddToggle = {openSkillsAddToggle} scrollToggle = {scrollToggle}/>
+
+
+        {/* skills add popup */}
+        {
+          isOpenSkillsAdd 
+          ?
+          <SkillsAdd skills = {skill} openSkillsAddToggle = {openSkillsAddToggle} scrollToggle = {scrollToggle}/>
+          :
+          null
+        }
     </div>
   )
 }
 
+function BasicInfo (props){
+  return(
+    <div className='mt-10 pt-1 md:pt-4 pl-4 w-[100%] md:w-[90%] gap:1 md:gap-4 md:rounded-lg  bg-[white] border-[1px] border-[#5d5d5d] flex flex-col justify-start items-center'>
+      <h3 className='font-semibold md:font-bold text-xl w-full pl-1 md:pl-4'>User Info</h3>   
+      <div className='pl-0 md:pl-4 w-full gap-4 py-8 rounded-lg  bg-[white] flex flex-col justify-start items-start'>
+        <p className='font-bold text-sm md:text-xl'>Role: {props.user.role}</p>
+        <p className='font-semibold text-sm md:text-lg'>Location: {props.user.location}</p>
+        <p className='font-semibold text-sm md:text-lg'>gender: {props.user.gender}</p>
+      </div>
 
-function Education (props){
-  
-const education = props.education;
-const education1 = props.education1;
-  return (
-        
-        <div className='mt-10 pt-1 md:pt-4 pl-4 w-[100%] md:w-[90%] gap:1 md:gap-4 md:rounded-lg  bg-[white] border-[1px] border-[#5d5d5d] flex flex-col justify-start items-center'>
-
-        <h3 className='font-semibold md:font-bold text-xl w-full pl-1 md:pl-4'>Education</h3>
-
-        {
-        education
-        ? 
-        <div className='pl-0 md:pl-4 w-full gap-4 py-8 rounded-lg  bg-[white] flex justify-start items-center'>
-          <div className='h-[50px] w-[50px] sm:h-[80px] sm:w-[80px] md:h-[150px] md:w-[150px] bg-[url(https://cdn-icons-png.freepik.com/256/4981/4981453.png?semt=ais_hybrid)] bg-cover bg-center '></div>
-          <div>
-            <p className='font-bold text-sm md:text-xl'>{education1.name}</p>
-            <p className='font-semibold text-xs md:text-lg'> {education1.degree} - {education1.field}</p>
-            <p className='font-semibold text-sm md:text-lg'>{education1.startYear} - {education1.endYear}</p>
-            <p className='font-semibold text-sm md:text-lg text-[#616161]'>{education1.location}</p>
-          </div>
-        </div>
-        :
-        <div className='w-[90%] my-8 h-[50px] md:h-[100px] rounded-lg border-1 outline-dashed bg-[#0b67c25a]
-        flex justify-center items-center text-[#0B65C2] font-semibold text-xl text-center'>+ Add Education</div> 
-        }
-        </div>
-)
+    </div>
+  )
 }
-
 
 function EditProfile (props){
   const [name, setName] = useState('');
@@ -153,7 +171,7 @@ function EditProfile (props){
   };
 
   return(
-    <div className='w-full h-[100vh] absolute top-0 left-0 bg-[#0000005a] flex justify-center items-center z-[100]'>
+    <div className='w-full h-[100vh] fixed top-0 left-0 bg-[#0000005a] flex justify-center items-center z-[100]'>
       <div className='w-full  md:w-[50%] z-[120] bg-white absolute md:top-[50%] md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%] md:rounded-xl border-[1px] border-[#5d5d5d] flex flex-col justify-start items-center shadow-lg'>
         <button className=' absolute top-0 right-0 h-[65px] w-[65px] rounded-tr-xl text-3xl flex justify-center items-center' onClick = {()=>{
           props.scrollToggle();
@@ -270,5 +288,100 @@ function EditProfile (props){
       </div>
     </div>
     
+  )
+}
+
+function Education (props){
+  
+const education = props.education;
+const education1 = props.education1;
+  return (
+        
+        <div className='mt-10 pt-1 md:pt-4 pl-4 w-[100%] md:w-[90%] gap:1 md:gap-4 md:rounded-lg  bg-[white] border-[1px] border-[#5d5d5d] flex flex-col justify-start items-center'>
+
+        <h3 className='font-semibold md:font-bold text-xl w-full pl-1 md:pl-4'>Education</h3>
+
+        {
+        education
+        ? 
+        <div className='pl-0 md:pl-4 w-full gap-4 py-8 rounded-lg  bg-[white] flex justify-start items-center'>
+          <div className='h-[50px] w-[50px] sm:h-[80px] sm:w-[80px] md:h-[150px] md:w-[150px] bg-[url(https://cdn-icons-png.freepik.com/256/4981/4981453.png?semt=ais_hybrid)] bg-cover bg-center '></div>
+          <div>
+            <p className='font-bold text-sm md:text-xl'>{education1[0].name}</p>
+            <p className='font-semibold text-xs md:text-lg'> {education1[0].degree} - {education1[0].field}</p>
+            <p className='font-semibold text-sm md:text-lg'>{education1[0].startYear} - {education1[0].endYear}</p>
+            <p className='font-semibold text-sm md:text-lg text-[#616161]'>{education1[0].location}</p>
+          </div>
+        </div>
+        :
+        <div className='w-[90%] my-8 h-[50px] md:h-[100px] rounded-lg border-1 outline-dashed bg-[#0b67c25a]
+        flex justify-center items-center text-[#0B65C2] font-semibold text-xl text-center'>+ Add Education</div> 
+        }
+        </div>
+)
+}
+
+// function EditEducation (props){
+//   return(
+//     <p>hellow</p>
+//   )
+// }
+
+function Skills (props){
+  return(
+    <div className='mt-10 pt-1 md:pt-4 pl-4 w-[100%] md:w-[90%] gap:1 md:gap-4 md:rounded-lg  bg-[white] border-[1px] border-[#5d5d5d] flex flex-col justify-start items-center'>
+      <h3 className='font-semibold md:font-bold text-xl w-full pl-1 md:pl-4'>Skills</h3>
+      {
+        props.skills.length > 0
+        ? 
+          <div className='pl-0 md:pl-4 w-full gap-4 py-8 rounded-lg  bg-[white] flex justify-start items-center flex-wrap'>
+            {
+              props.skills.map((skills)=>{
+              
+                return(
+                  <p className='h-[40px] bg-[#0B65C2] p-2 text-white rounded-lg'>{skills}</p>
+                )})
+            }
+            <button onClick={()=>{
+              props.openSkillsAddToggle();
+              props.scrollToggle();
+            }} className='h-[40px] p-2 rounded-lg border-1 outline-dashed bg-[#0b67c25a]
+            flex justify-center items-center text-[#0B65C2] font-semibold text-xl text-center'>+ Add Skills</button>
+          </div>
+
+        : 
+        <button className='w-[90%] my-8 h-[50px] md:h-[100px] rounded-lg border-1 outline-dashed bg-[#0b67c25a]
+        flex justify-center items-center text-[#0B65C2] font-semibold text-xl text-center'>+ Add Skills</button>
+      }
+    </div>
+  )
+}
+
+function SkillsAdd (props){
+  return(
+    <div className='w-full h-[100vh] fixed top-0 left-0 bg-[#0000005a] flex justify-center items-center z-[100]'>
+      <div className='w-full  md:w-[50%] z-[120] bg-white absolute md:top-[50%] md:left-[50%] md:translate-x-[-50%] md:translate-y-[-50%] md:rounded-xl border-[1px] border-[#5d5d5d] flex flex-col justify-start items-center shadow-lg'>
+        <button onClick={()=>{
+          props.openSkillsAddToggle();
+          props.scrollToggle();
+        }} className=' absolute top-0 right-0 h-[65px] w-[65px] rounded-tr-xl text-3xl flex justify-center items-center'><IoMdClose />
+        </button>
+        <p className='w-full h-[50px] pl-4 text-2xl font-bold mt-[15px] border-b-[1px] border-[#5d5d5d]'>Add Skills</p>
+        
+        <div className='pl-0 md:pl-4 w-full gap-4 py-8 rounded-lg  bg-[white] flex justify-start items-center flex-wrap'>
+          {
+            props.skills.map((skills)=>{
+              return (
+            <div>
+                <p className='h-[40px] bg-[#0B65C2] p-2 text-white rounded-lg'>{skills}</p>
+              </div>
+              )
+
+            })
+          }
+        </div>
+
+      </div>
+    </div>
   )
 }
